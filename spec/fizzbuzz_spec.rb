0591138fab0cd834cd7c06c3c9659ce525e59bc3
Cam Woloshyn <camw@nulogy.com>
2017-01-29 10:35:49 -0500

    Refactors tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,21 +1,23 @@
 require "fizzbuzz"
 
 RSpec.describe FizzBuzz do
-  it "return 'fizz' when passed 3" do
-    result = FizzBuzz.calculate(3)
+  describe "fizz numbers" do
+    it "returns 'fizz' when passed 3" do
+      result = FizzBuzz.calculate(3)
 
-    expect(result).to eq("fizz")
+      expect(result).to eq("fizz")
+    end
+
+    it "returns 'fizz' for an arbitrary multiple of 3" do
+      result = FizzBuzz.calculate(12)
+
+      expect(result).to eq("fizz")
+    end
   end
 
   it "returns 'buzz' when passed 5" do
     result = FizzBuzz.calculate(5)
 
     expect(result).to eq("buzz")
-  end
-
-  it "returns 'fizz' for an arbitrary multiple of 3" do
-    result = FizzBuzz.calculate(12)
-
-    expect(result).to eq("fizz")
   end
 end
