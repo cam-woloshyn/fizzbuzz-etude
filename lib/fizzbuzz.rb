f8a4fd2982726c61bf02af4941a274a58c6bb050
Cam Woloshyn <camw@nulogy.com>
2017-01-29 10:45:31 -0500

    Refactor: extract methods

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,17 @@
 module FizzBuzz
   def self.calculate(number)
-    return "fizzbuzz" if (number % 3).zero? && (number % 5).zero?
-    return "fizz" if (number % 3).zero?
-    return "buzz" if (number % 5).zero?
+    return "fizzbuzz" if fizz?(number) && buzz?(number)
+    return "fizz" if fizz?(number)
+    return "buzz" if buzz?(number)
 
     number.to_s
+  end
+
+  def self.buzz?(number)
+    (number % 5).zero?
+  end
+
+  def self.fizz?(number)
+    (number % 3).zero?
   end
 end
