7b14a54598bff9b1993c6e9e670818af5c246152
Cam Woloshyn <camw@nulogy.com>
2017-01-29 10:23:15 -0500

    First passing test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,13 @@
 module FizzBuzz
+  def self.calculate(_number)
+    "fizz"
+  end
 end
 
 RSpec.describe FizzBuzz do
+  it "return 'fizz' when passed 3" do
+    result = FizzBuzz.calculate(3)
+
+    expect(result).to eq("fizz")
+  end
 end
