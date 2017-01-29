76898c68bbffe5e7f82e3837c4035aa658b20e1b
Cam Woloshyn <camw@nulogy.com>
2017-01-29 10:37:59 -0500

    Handles arbitrary buzz numbers

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -15,9 +15,17 @@ RSpec.describe FizzBuzz do
     end
   end
 
-  it "returns 'buzz' when passed 5" do
-    result = FizzBuzz.calculate(5)
+  describe "buzz numbers" do
+    it "returns 'buzz' when passed 5" do
+      result = FizzBuzz.calculate(5)
 
-    expect(result).to eq("buzz")
+      expect(result).to eq("buzz")
+    end
+
+    it "returns 'buzz' for an arbitrary multiple of 5" do
+      result = FizzBuzz.calculate(20)
+
+      expect(result).to eq("buzz")
+    end
   end
 end
