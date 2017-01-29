2096c20d5c85cc87d58ff3857db34abdb9b656df
Cam Woloshyn <camw@nulogy.com>
2017-01-29 10:38:24 -0500

    Refactors implementation

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,6 @@
 module FizzBuzz
   def self.calculate(number)
     return "fizz" if (number % 3).zero?
-    return "buzz" if number == 5 || number == 20
+    return "buzz" if (number % 5).zero?
   end
 end
